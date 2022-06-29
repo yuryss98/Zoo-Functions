593a89beead83f841b7ec51ad6b950a639a3985a
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-06-29 15:56:58 -0300

    requisito 2 completo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  return species.find((element) => element.name === animal).residents.every(
+    (element) => element.age >= age,
+  );
 }
 
 module.exports = getAnimalsOlderThan;
