3d44259d1f5786f83ae6e79667e5981d9156186c
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-06-29 18:26:21 -0300

    requisito 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  if (!animal.sex) {
+    return species.find((element) => element.name === animal.specie).residents.length;
+  }
+  return species.find((element) => element.name === animal.specie).residents.filter(
+    (element) => (element.sex === animal.sex),
+  ).length;
 }
-
 module.exports = countAnimals;
