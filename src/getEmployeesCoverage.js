0a041ead1e472b7ca6583bda2d26a8b4289185dc
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-06-30 19:07:09 -0300

    requisito 11 completo

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,59 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+const infoDaPessoaEncontrada = (obj) => {
+  const name = Object.values(obj);
+  const pessoaEncontrada = employees.find((element) => element.id === name[0]
+  || element.firstName === name[0] || element.lastName === name[0]);
+  if (!pessoaEncontrada) {
+    throw new Error('Informações inválidas');
+  }
+  const responsabilidade = pessoaEncontrada.responsibleFor;
+  const arrSpecies = [];
+  responsabilidade.forEach((element) => {
+    const animais = species.find((animal) => animal.id === element).name;
+    arrSpecies.push(animais);
+  });
+  return {
+    pessoaEncontrada,
+    responsabilidade,
+    arrSpecies,
+  };
+};
+
+const localizacaoAnimais = (obj) => {
+  const test = infoDaPessoaEncontrada(obj);
+  const arrLocation = [];
+  test.arrSpecies.forEach((element) => {
+    arrLocation.push(species.find((animal) => animal.name === element).location);
+  });
+  return arrLocation;
+};
+
+const infoDeTodasAsPessoas = () => {
+  return employees.map((element) => {
+    const test = infoDaPessoaEncontrada(element);
+    const test2 = localizacaoAnimais(element);
+    return {
+      id: test.pessoaEncontrada.id,
+      fullName: `${test.pessoaEncontrada.firstName} ${test.pessoaEncontrada.lastName}`,
+      species: test.arrSpecies,
+      locations: test2,
+    };
+  });
+};
+
+function getEmployeesCoverage(obj) {
+  if (!obj) return infoDeTodasAsPessoas();
+  const test = infoDaPessoaEncontrada(obj);
+  const test2 = localizacaoAnimais(obj);
+  return {
+    id: test.pessoaEncontrada.id,
+    fullName: `${test.pessoaEncontrada.firstName} ${test.pessoaEncontrada.lastName}`,
+    species: test.arrSpecies,
+    locations: test2,
+  };
 }
 
 module.exports = getEmployeesCoverage;
