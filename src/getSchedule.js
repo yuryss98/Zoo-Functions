7838e7c6aa7046a59f65db0a73a1f6f314a1dc57
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-06-30 16:43:20 -0300

    requisito 9 completo

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
 
+const { hours, species } = data;
+
+const exibicaoDoAnimal = (animal) => {
+  const animalName = species.find((element) => element.name === animal);
+  return animalName.availability;
+};
+
+function todosOsDiasDaSemana(param) {
+  const dias = Object.keys(hours);
+  const obj = {};
+  dias.forEach((element) => {
+    obj[element] = {
+      officeHour: `Open from ${hours[element].open}am until ${hours[element].close}pm`,
+      exhibition: species.filter((dia) => dia.availability.includes(element)).map(
+        (dia) => dia.name,
+      ),
+    };
+  });
+  obj.Monday = { officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!' };
+  return obj;
+}
+
+const exibicaoDoDia = (dia) => {
+  const dias = Object.keys(hours);
+  if (dias.includes(dia)) {
+    const diasRetornados = todosOsDiasDaSemana();
+    return {
+      [dia]: diasRetornados[dia],
+    };
+  }
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animal = species.some((element) => element.name.includes(scheduleTarget));
+  const diasSemana = Object.keys(hours);
+  if (animal) {
+    return exibicaoDoAnimal(scheduleTarget);
+  }
+  if (diasSemana.includes(scheduleTarget)) {
+    return exibicaoDoDia(scheduleTarget);
+  }
+  return todosOsDiasDaSemana(scheduleTarget);
 }
 
 module.exports = getSchedule;
