bc2351cd6ae22ba49f21d996525e484436e06ef6
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-06-29 22:15:58 -0300

    requisito 8 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aquii
+  return {
+    child: entrants.filter((element) => element.age < 18).length,
+    adult: entrants.filter((element) => element.age >= 18 && element.age < 50).length,
+    senior: entrants.filter((element) => element.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) return 0;
+  if (!Object.keys(entrants).length) return 0;
+  const { prices } = data;
+  const quantidadeDePessoas = countEntrants(entrants);
+  const child = quantidadeDePessoas.child * prices.child;
+  const adult = quantidadeDePessoas.adult * prices.adult;
+  const senior = quantidadeDePessoas.senior * prices.senior;
+  return child + adult + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
