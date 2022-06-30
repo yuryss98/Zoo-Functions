74c08406dfd14971626c1e4a7cde4ec42cf7a53e
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-06-29 21:14:32 -0300

    requisito 7 completo

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,68 +1,4 @@
-const { handlerElephants, getElephants, averageAge, computeData } = require('../src/handlerElephants');
-
-describe('testes da função getElephants', () => {
-  it('testa se ela e de fato uma função', () => {
-    expect(typeof getElephants).toBe('function');
-  });
-  it('verifique se ela retorna todo o objeto do elefante', () => {
-    expect(getElephants()).toEqual({
-      id: 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-      name: 'elephants',
-      popularity: 5,
-      location: 'NW',
-      availability: ['Friday', 'Saturday', 'Sunday', 'Tuesday'],
-      residents: [
-        {
-          name: 'Ilana',
-          sex: 'female',
-          age: 11,
-        },
-        {
-          name: 'Orval',
-          sex: 'male',
-          age: 15,
-        },
-        {
-          name: 'Bea',
-          sex: 'female',
-          age: 12,
-        },
-        {
-          name: 'Jefferson',
-          sex: 'male',
-          age: 4,
-        },
-      ],
-    });
-  });
-});
-
-describe('Testes da função averageAge', () => {
-  it('testa se é uma função', () => {
-    expect(typeof averageAge).toBe('function');
-  });
-  it('teste se ela retorna a média de idade dos elefantes', () => {
-    const retornoDeGetElephants = getElephants();
-    // const { residents } = retornoDeGetElephants;
-    expect(averageAge(retornoDeGetElephants)).toBeCloseTo(10.5);
-  });
-});
-
-describe('Testes da função computeData', () => {
-  const retornoDeGetElephants = getElephants();
-  it('testa se ela e uma função', () => {
-    expect(typeof computeData).toBe('function');
-  });
-  it('teste se ao passar os parametro ("count", retornoDeGetElephants) retorna 4', () => {
-    expect(computeData('count', retornoDeGetElephants)).toBe(4);
-  });
-  it('testa se ao passar os parametros ("avarage", retornoDeGetElephants) retorna 10.5', () => {
-    expect(computeData('averageAge', retornoDeGetElephants)).toBeCloseTo(10.5);
-  });
-  it('testa se ao passar um parametro desconhecido retorna null', () => {
-    expect(computeData('desconhecido', retornoDeGetElephants)).toBeNull();
-  });
-});
+const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
   it('testa se ela e uma função', () => {
@@ -79,5 +15,14 @@ describe('Testes da função HandlerElephants', () => {
   });
   it('testa se ao passar o parametro "names" retorna o seguinte array ["Ilana", "Orval", "Bea", "Jefferson"]', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('teste se passar o parametro (averageAge) ela retorna 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('teste se passar o parametro (count) ela retorna 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('testa se ao passar um parametro desconhecido retorna null', () => {
+    expect(handlerElephants('desconhecido')).toBeNull();
   });
 });
