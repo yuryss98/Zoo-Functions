bcce371e50bf18bcd8330c6a62e29feadaf80418
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-06-30 17:12:52 -0300

    requisito 10 completo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const { species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colaboradora = employees.find((element) => element.id === id);
+  const primeiraEspecie = colaboradora.responsibleFor[0];
+  const infosDoAnimal = species.find((specie) => specie.id === primeiraEspecie).residents;
+  const ordenadosPorIdade = infosDoAnimal.sort((a, b) => a.age - b.age);
+  const maisVelho = ordenadosPorIdade[ordenadosPorIdade.length - 1];
+  const { name, sex, age } = maisVelho;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
